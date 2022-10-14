8961ca353eed75a7f1cd98759c5ee5def04c8e2b
karim <k.harazeen@gmail.com>
2022-10-14 22:33:06 +0300

    rename NewsApiTest

--- tests/Feature/NewsApiRouteTest.php
+++ tests/Feature/NewsApiRouteTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class NewsApiTest extends TestCase
+class NewsApiRouteTest extends TestCase
 {
     use RefreshDatabase;
 
